c7268557fb178370e322bc2dbda13b496bce8743
MFvanRossum <matissev@hotmail.com>
2020-02-19 19:09:51 -0500

    Computer and Player can take turns. Die roll is random every time

--- pig.py
+++ pig.py
@@ -8,7 +8,6 @@ class Game:
 
     def choose_starting_player(self):
         rng = randint(1, 10)
-        print(f"{rng}")
         if rng < 6:
             print("Computer goes first.")
             self.computer.round()
@@ -29,31 +28,31 @@ class Computer:
         self.name = name 
         self.round_score = 0
         self.computer_score = 0
-        self.roll_die = Die()
 
 
     def __str__(self):
         return f"{self.name}"
 
     def round(self):
-        self.roll_die.value
-        print(f"Computer rolls {self.roll_die.value}")
-        if self.roll_die.value != 1:
-            if self.round_score < 20:
-                self.round_score += self.roll_die.value
+        if self.round_score < 20:
+            roll = Die().value
+            print(f"Computer rolls {roll}")
+            if roll != 1:
+                self.round_score += roll
                 print(f"Round score is {self.round_score}")
-                self.roll_die.value
-                print(f"Computer rolls {self.roll_die.value}")
-            else: 
-                # self.hold
+                self.round()
+            else:
+                print("Pig!") 
+                self.round_score = 0
+                self.computer_score += self.round_score
+                print(f"Computer's total score is {self.computer_score}")
                 print("Computer holds")
-                self.round_score += self.computer_score
+                self.computer_score += self.round_score
                 print(f"Computer's total score is {self.computer_score}")
         else: 
-            print("Pig!")
-            self.round_score = 0
-            self.round_score += self.computer_score
-            print(f"Computer's total score is {self.computer_score")
+            print("Computer holds")
+            self.computer_score += self.round_score
+            print(f"Computer's total score is {self.computer_score}")
 
 
 class Player:
@@ -61,7 +60,6 @@ class Player:
         self.name = name 
         self.round_score = 0
         self.player_score = 0
-        self.roll_die = Die()
 
     def __str__(self):
         return f"{self.name}"
@@ -69,14 +67,14 @@ class Player:
     def round(self):
         choice = input("Do you want to (r)oll or (h)old? ")
         while choice != 'h':
-            self.roll_die.value
-            print(f"You rolled {self.roll_die.value}")
-            self.round_score += self.roll_die.value
+            roll = Die().value
+            print(f"You rolled {roll}")
+            self.round_score += roll
             print(f"Your score for this round is {self.round_score}")
             choice = input("Do you want to (r)oll or (h)old? ")
-            if choice == "h": 
-                self.round_score += self.player_score
-                print(f"Your total score is {self.player_score}")
+        if choice == "h": 
+            self.player_score += self.round_score
+            print(f"Your total score is {self.player_score}")
 
 
 game = Game()
